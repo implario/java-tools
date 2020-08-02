3d248b117722c3187552fe9be8d0f944ac35e4c2
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2020-08-02 08:59:07 +0300

    Added some tests for plurals() method

--- src/main/java/clepto/humanize/Humanize.java
+++ src/main/java/clepto/humanize/Humanize.java
@@ -2,9 +2,10 @@ package clepto.humanize;
 
 public class Humanize {
 
-	public static String plurals(String one, String couple, String many, int n) {
+	public static String plurals(String one, String two, String five, int n) {
+		if (n < 0) n = -n;
 		return n % 10 == 1 && n % 100 != 11 ? one :
-				n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? couple : many;
+				n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? two : five;
 	}
 
 }
